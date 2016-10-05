d198177f2bcfc96f2d87c20c9f2518afa2916fa5
aguptaad <aguptaad@ust.hk>
2016-10-05 12:18:50 +0800

    LAB5

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -1,15 +1,38 @@
 package base;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class NoteBook {
+public class NoteBook implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private ArrayList<Folder> folders;
 	public NoteBook(){
 		this.folders = new ArrayList<Folder>();
 	}
-	
+	public NoteBook(String file){
+
+		FileInputStream fis =null;
+		ObjectInputStream in = null;
+		try{
+			fis=new FileInputStream(file);
+			in=new ObjectInputStream(fis);
+			NoteBook n =(NoteBook) in.readObject();
+			in.close();
+			this.folders = n.getFolders();
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
 	private boolean insertNote(String folderName, Note note) { 
 		Folder f = null; 
 		boolean checkFolder = false;
@@ -20,20 +43,20 @@ public class NoteBook {
 				f = f1;
 			}
 		} 
-		
+
 		if (f == null && checkFolder == false) {
 			f = new Folder(folderName);
 			folders.add(f);
 		} 
 		else{
 		}
-		
+
 		for (Note n : f.getNotes()) { 
 			if(note.getTitle().equals(n.getTitle())){
 				checkNote = true;
 			}
 		}
-		
+
 		if(checkNote){
 			System.out.println("Creating note: " + note.getTitle()+" under folder " + folderName + " failed");
 		}
@@ -42,32 +65,32 @@ public class NoteBook {
 		}
 		return !checkNote;
 	}
-			
-	
+
+
 	public boolean createTextNote(String folderName, String title) { 
 		TextNote note = new TextNote(title); 
 		return insertNote(folderName, note); 
-		}
-	
+	}
+
 	public boolean createTextNote(String folderName, String title, String content) { 
 		TextNote note = new TextNote(title, content); 
 		return insertNote(folderName, note); 
-		}
+	}
 
 	public boolean createImageNote(String folderName, String title) { 
 		ImageNote note = new ImageNote(title); 
 		return insertNote(folderName, note);  
-		}
-	
+	}
+
 	public ArrayList<Folder> getFolders() { return folders; }
-	
+
 	public void sortFolders(){
 		for (Folder f: folders){
 			f.sortNotes();
 		}
 		Collections.sort(folders);
 	}
-	
+
 	public List<Note> searchNotes(String Keywords){
 		ArrayList<Note> newNotes = new ArrayList<Note>();
 		for (Folder f: folders){
@@ -76,5 +99,21 @@ public class NoteBook {
 			}
 		}
 		return newNotes;
+	}
+
+	public boolean save(String file){
+		NoteBook object = new NoteBook();
+		FileOutputStream fos = null;
+		ObjectOutputStream out=null;
+		try{
+			fos=new FileOutputStream(file);
+			out=new ObjectOutputStream(fos);
+			out.writeObject(object);
+			out.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return true;
 	}
 }
--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,5 +1,11 @@
 package base;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class TextNote extends Note {
 	private String content;
 	public TextNote(String title) {
@@ -15,6 +21,54 @@ public class TextNote extends Note {
 	}
 	public void setContent(String content) {
 		this.content = content;
+	}
+	
+	public TextNote(File f) {
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+	}
+	
+	private String getTextFromFile(String absolutePath){
+		StringBuffer result = new StringBuffer("");
+		File file = new File(absolutePath);
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(file);
+
+			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				result.append((char) content);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		return result.toString();
+	}
+	
+	public void exportTextToFile(String pathFolder){
+		String title = this.getTitle().replaceAll(" ","_");
+		File file = new File(pathFolder + title + ".txt");
+		try{
+			FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
+			ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.writeObject(this.content);
+			out.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
